6dbbbc3d0c940e304750d6811df9fa4e58784434
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 12:03:40 +0530

    added tuple

---
+++ 004_tuple.py
@@ -0,0 +1,17 @@
+#TUPLE
+# (), duplicates allowed, ordered, index, immutable
+
+my_tuple = (1, 3, 5, 2, 8, 8)
+
+# print tuple
+print(my_tuple)
+
+# first element of tuple
+print(my_tuple[0])
+
+# second element of tuple
+print(my_tuple[1])
+
+# assign first element 1000
+# my_tuple[0] = 1000
+# print(my_tuple)
