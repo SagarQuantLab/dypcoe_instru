a0de030f338e4c62e086fc8deca690e05b9599ad
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 11:59:45 +0530

    added dict

---
+++ 003_dict.py
@@ -0,0 +1,19 @@
+#DICT
+# {}, keys, no duplicates allowed, ordered, mutable
+
+
+my_dict = {
+    "Name":"Rohan",
+    "Age":35,
+    "Gender":"Male",
+    "Name" : "Sohan"
+}
+
+#access 'Rohan'
+print(my_dict["Name"])
+
+print(my_dict)
+
+# update age to 25
+my_dict["Age"] = 25
+print(my_dict)
