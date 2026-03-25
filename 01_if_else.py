dcc4d77a2dab49f61a944a6f761f7c24420ab01f
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 12:38:17 +0530

    added if else

---
+++ 01_if_else.py
@@ -0,0 +1,30 @@
+# IF ELSE
+
+my_age = 19
+
+# if block
+if my_age>18:
+    print("Adult")
+
+# if else
+if my_age> 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# if elif else
+if my_age> 18:
+    print("Adult")
+elif my_age == 18:
+    print("Turning Adult")
+else:
+    print("Minor")
+
+
+# code reduction
+my_age = 18
+msg = "Bye"
+if my_age > 18:
+    msg = "hello"
+
+print(msg)
