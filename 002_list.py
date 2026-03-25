55802d8cb4c561d72c544b615d83032eb78451ba
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 11:52:38 +0530

    added list

---
+++ 002_list.py
@@ -0,0 +1,23 @@
+#LIST
+# [], index, ordered, duplicates allowed, mutable
+
+
+my_list = [1, 3, 5, 2, 8, 8]
+
+# access first element
+print(my_list[0])
+
+# access second element
+print(my_list[1])
+
+# last element access
+print(my_list[-1])
+
+# second last element access
+print(my_list[-2])
+
+print(my_list)
+
+# update first element
+my_list[0] = 1000
+print(my_list)
